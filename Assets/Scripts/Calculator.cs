96438c0bfeddf2eb1398dd171346601924f8c1f3
Ensar Balık <ensarbalk@gmail.com>
2023-07-17 14:29:20 +0300

    Calculator

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Calculator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject create;
+
+    public List<GameObject> testList;
+
+    public float radius = 1f;
+    public int amount;
+    public float frequency = 0.1f;
+    public int count;
+
+
+    private void Update()
     {
-        
+        Calculate();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateObject();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Calculate()
     {
-        
+        amount = testList.Count;
+
+        for (int i = 0; i < amount; i++)
+        {
+            var angle = i * Mathf.PI * 2f / amount;
+            float angleXYZ = Mathf.Sin(angle) * radius;
+            var newPos = new Vector3(Mathf.Cos(angle) * radius, angleXYZ, angleXYZ);
+            testList[i].transform.localPosition = newPos;
+        }
+    }
+
+    private void CreateObject()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            var test = Instantiate(create);
+            test.transform.parent = transform;
+            testList.Add(test);
+        }
     }
 }

8204625ade20ff76887d2301de4b2808a4361fe9
Ensar Balık <ensarbalk@gmail.com>
2023-07-28 11:46:43 +0300

    Grid system & data base implement

--- Assets/Scripts/Grid System/GridXZ.cs
+++ Assets/Scripts/Grid System/GridXZ.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GridXZ : MonoBehaviour
 {
@@ -10,6 +12,19 @@ public class GridXZ : MonoBehaviour
     private Vector3 lastPosition;
 
     [SerializeField] private LayerMask placementLayerMask;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnClicked?.Invoke();
+        if (Input.GetKeyDown(KeyCode.A))
+            OnExit?.Invoke();
+    }
+
+    public bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
+
 
     public Vector3 GetSelectedMapPosition()
     {
--- Assets/Scripts/Grid System/PlacementSystem.cs
+++ Assets/Scripts/Grid System/PlacementSystem.cs
@@ -2,16 +2,71 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField] private GameObject mouseTest, cellIndicator;
-    [SerializeField] private GridXZ gridXZ;
+
+    [FormerlySerializedAs("gridXZ")] [SerializeField]
+    private GridXZ inputManager;
+
     [SerializeField] private Grid grid;
+
+    [SerializeField] private ObjectsDatabaseSO database;
+    private int selectedObjectIndex = -1;
+
+    [SerializeField] private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedObjectIndex = database.ObjectDatas.FindIndex(data => data.ID == ID);
+        if (selectedObjectIndex < 0)
+        {
+            Debug.LogError($"NO ID FOUND {ID} ");
+            return;
+        }
+
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+
+        Vector3 mousePos = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPos = grid.WorldToCell(mousePos);
+        GameObject newObject = Instantiate(database.ObjectDatas[selectedObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPos);
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
 
     private void Update()
     {
-        Vector3 mousePos = gridXZ.GetSelectedMapPosition();
+        if (selectedObjectIndex < 0)
+            return;
+        Vector3 mousePos = inputManager.GetSelectedMapPosition();
         Vector3Int gridPos = grid.WorldToCell(mousePos);
         mouseTest.transform.position = mousePos;
         cellIndicator.transform.position = grid.CellToWorld(gridPos);
